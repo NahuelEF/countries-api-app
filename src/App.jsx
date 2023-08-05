dc2bd1bc5eee55cc1e09f995132120c8c0bc0c9d
NahuelEF <nahuelfedele@gmail.com>
2023-08-05 02:30:20 -0300

    add: navbar and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Header, Search, Filter } from "@/components";
+import "./App.scss";
+
+const options = [
+  { value: "africa", label: "Africa" },
+  { value: "america", label: "America" },
+  { value: "asia", label: "Asia" },
+  { value: "europe", label: "Europe" },
+  { value: "oceania", label: "Oceania" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <main className="main">
+        <nav className="nav">
+          <Search />
+          <Filter options={options} label="Filter by Region" />
+        </nav>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { Header } from "./Header/Header";
+export { Search } from "./Search/Search";
+export { Filter } from "./Filter/Filter";

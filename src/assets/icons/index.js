e7bb2f54aac9b268e0a03935a84b631ec40be9dd
NahuelEF <nahuelfedele@gmail.com>
2023-08-05 22:58:01 -0300

    add: component icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,4 @@
+export { ArrowDown as IconArrowDown } from "./ArrowDown";
+export { Moon as IconMoon } from "./Moon";
+export { MoonFill as IconMoonFill } from "./MoonFill";
+export { Search as IconSearch } from "./Search";

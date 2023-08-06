7847c293bef6a3fad574b32543a351497d4e13a9
NahuelEF <nahuelfedele@gmail.com>
2023-08-06 01:11:35 -0300

    fix: px to rem some style components

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,4 @@
+import { IconArrowDown } from "@/assets/icons";
 import { useEffect, useState } from "react";
 import style from "./Filter.module.scss";
 
@@ -35,12 +36,7 @@ export const Filter = ({ label, options }) => {
       <div onClick={handleClick} className={style.filterLabel}>
         {activeLabel}
         <span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path
-              fillRule="evenodd"
-              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-            />
-          </svg>
+          <IconArrowDown />
         </span>
       </div>
       {showMenu && (

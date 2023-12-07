69b1587dffb59e82262f0680d8fcc10702240390
NahuelEF <nahuelfedele@gmail.com>
2023-12-07 02:35:12 -0300

    fix: component icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,4 @@
-export { ArrowDown as IconArrowDown } from "./ArrowDown";
-export { Moon as IconMoon } from "./Moon";
-export { MoonFill as IconMoonFill } from "./MoonFill";
-export { Search as IconSearch } from "./Search";
+export * from './ArrowDown';
+export * from './Moon';
+export * from './MoonFill';
+export * from './Search';

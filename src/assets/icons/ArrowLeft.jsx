f300651fe4a6d4eebf098904ece4ea6a1ae7f88c
NahuelEF <nahuelfedele@gmail.com>
2023-12-31 03:00:28 -0300

    fix: arrow left icon

---
+++ src/assets/icons/ArrowLeft.jsx
@@ -0,0 +1,11 @@
+export const ArrowLeftIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none">
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M21 12H3M8 17l-5-5 5-5"
+    />
+  </svg>
+);

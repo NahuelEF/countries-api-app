0dfef2dca99617fe91fe5b17d2b346de69017862
NahuelEF <nahuelfedele@gmail.com>
2023-12-31 03:18:19 -0300

    fix: countries fetch function with loader

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,43 +1,44 @@
-import { useFetch } from "@/hooks/useFetch";
-import { Navbar } from "@/layout";
-import { formatNumber } from "@/utils/formatNumber";
-import { Link } from "react-router-dom";
+import { Suspense } from "react";
+import { Await, defer, useLoaderData } from "react-router-dom";
+import { ArrowDownIcon } from "@/assets/icons";
+import { Card } from "@/components";
+import { Navbar } from "@/layouts";
+import { fetchData } from "@/utils";
 import style from "./Home.module.scss";
 
-/* const countryName = "argentina";
-const url = `https://restcountries.com/v3.1/name/${countryName}`; */
+//https://restcountries.com/
 
-const region = "europe";
-const url = `https://restcountries.com/v3.1/region/${region}`;
+export const homeLoader = () => {
+  const url = `https://restcountries.com/v3.1/all?fields=flags,name,population,region,capital,cca3`;
+
+  const countries = fetchData(url);
+
+  return defer({ countries });
+};
 
 export const Home = () => {
-  const { data, loading } = useFetch(url);
+  const { countries } = useLoaderData();
 
   return (
-    <main className={style.main}>
+    <>
       <Navbar />
       <section className={style.section}>
-        {loading && <h2>Loading...</h2>}
-        {data?.map(({ cca2, flags, name, population, region, capital }) => (
-          <Link key={cca2} to={`country/${name.common}`} className={style.card}>
-            <img className={style.cardImage} src={flags.png} alt={`Flag of ${name.common}`} />
-            <div className={style.cardContainer}>
-              <h2 className={style.cardTitle}>{name.common}</h2>
-              <ul className={style.cardList}>
-                <li>
-                  <strong className={style.cardStrong}>Population:</strong> {formatNumber(population)}
-                </li>
-                <li>
-                  <strong className={style.cardStrong}>Region:</strong> {region}
-                </li>
-                <li>
-                  <strong className={style.cardStrong}>Capital:</strong> {capital}
-                </li>
-              </ul>
-            </div>
-          </Link>
-        ))}
+        <Suspense fallback={<h2>Loading countries...</h2>}>
+          <Await resolve={countries}>
+            <Card />
+          </Await>
+        </Suspense>
       </section>
-    </main>
+      <button
+        className={style.button}
+        type="button"
+        title="Go to top"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        <span>
+          <ArrowDownIcon />
+        </span>
+      </button>
+    </>
   );
 };

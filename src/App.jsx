496c26dcc1e64b8e7c0221e9167da3220b9b38d7
NahuelEF <nahuelfedele@gmail.com>
2024-01-03 00:27:55 -0300

    fix: routes to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import { route } from "./routes";
-import "./App.scss";
 
 export default function App() {
   return <RouterProvider router={route} />;
